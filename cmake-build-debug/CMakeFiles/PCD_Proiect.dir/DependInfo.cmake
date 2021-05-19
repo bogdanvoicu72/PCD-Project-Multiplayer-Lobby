0704005fb9e579333ef0b42ebdc515b3c732c23a
Bogdan Voicu <bogdanvoicu@Bogdans-MacBook-Pro.local>
2021-05-19 23:15:46 +0300

    unu sau mai multi clienti server cu fork

---
+++ cmake-build-debug/CMakeFiles/PCD_Proiect.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/bogdanvoicu/CLionProjects/PCD-Proiect/client.c" "/Users/bogdanvoicu/CLionProjects/PCD-Proiect/cmake-build-debug/CMakeFiles/PCD_Proiect.dir/client.c.o"
+  "/Users/bogdanvoicu/CLionProjects/PCD-Proiect/main.c" "/Users/bogdanvoicu/CLionProjects/PCD-Proiect/cmake-build-debug/CMakeFiles/PCD_Proiect.dir/main.c.o"
+  "/Users/bogdanvoicu/CLionProjects/PCD-Proiect/server.c" "/Users/bogdanvoicu/CLionProjects/PCD-Proiect/cmake-build-debug/CMakeFiles/PCD_Proiect.dir/server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PCD_Proiect.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCD_Proiect.dir/client.c.o"
+  "CMakeFiles/PCD_Proiect.dir/main.c.o"
+  "CMakeFiles/PCD_Proiect.dir/server.c.o"
+  "PCD_Proiect"
+  "PCD_Proiect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PCD_Proiect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
